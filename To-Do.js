2b9ba577d7377e72559c89ef2b4d4102669f3ece
James Hirschfelder <james.hirschfelder@gmail.com>
2021-05-06 12:07:14 -0700

    Delete button object added

--- To-Do.js
+++ To-Do.js
@@ -5,12 +5,21 @@ button.addEventListener('click',theNewTask);
 function theNewTask(e){
   e.preventDefault();
 
+    //add if blank styling
+  //if (taskInput === "") {.
+
   const allTasks = document.getElementById("taskList")
   const newTask = document.getElementById("taskName").value;
   const li = document.createElement("li");
-  const myNode = document.createTextNode(newTask);
 
+
+  const deleteButton=document.createElement("button");
+  deleteButton.innerHTML="X";
+  deleteButton.setAttribute("id", "myDeleteButton");
+
+  const myNode = document.createTextNode(newTask);
   li.appendChild(myNode);
+  li.appendChild(deleteButton);
 
   allTasks.appendChild(li);
   document.getElementById("myForm").reset();

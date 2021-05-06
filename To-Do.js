572ce8920169508e9ac39e0e01e99f0966c095da
James Hirschfelder <james.hirschfelder@gmail.com>
2021-05-06 12:17:41 -0700

    Delete functionality added

--- To-Do.js
+++ To-Do.js
@@ -16,10 +16,14 @@ function theNewTask(e){
   const deleteButton=document.createElement("button");
   deleteButton.innerHTML="X";
   deleteButton.setAttribute("id", "myDeleteButton");
+  deleteButton.onclick = function() {
+    li.style.display = "none";
+  }
 
   const myNode = document.createTextNode(newTask);
-  li.appendChild(myNode);
   li.appendChild(deleteButton);
+  li.appendChild(myNode);
+
 
   allTasks.appendChild(li);
   document.getElementById("myForm").reset();

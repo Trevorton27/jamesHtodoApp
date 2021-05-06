4b20d9955dd82c9bb6b184f971c12b96610fdc49
James Hirschfelder <james.hirschfelder@gmail.com>
2021-05-06 16:54:54 -0700

    Reworked button functionality, added UID's

--- To-Do.js
+++ To-Do.js
@@ -2,47 +2,65 @@ const button = document.getElementById("mybutton");
 button.addEventListener('click',theNewTask);
 
 
+
+
 function theNewTask(e){
   e.preventDefault();
 
     //add if blank styling
   //if (taskInput === "") {.
+  //add if duplicate alert
+  //} else {
 
   const allTasks = document.getElementById("taskList")
   const newTask = document.getElementById("taskName").value;
   
   const li = document.createElement("li");
-  li.setAttribute("id", "listItem");
+  li.setAttribute("id", Date.now());
 
   const deleteButton=document.createElement("button");
   deleteButton.innerHTML="X";
-  deleteButton.setAttribute("id", "myDeleteButton");
-  deleteButton.onclick = function() {
-    li.style.display = "none";
-  }
+  deleteButton.setAttribute("name", "theDeleteButtons");
+  deleteButton.setAttribute("id", li.id);
+  deleteButton.setAttribute("onClick","deleteItem(this.id)");
+
 
   const completeButton=document.createElement("input");
   completeButton.setAttribute("type", "checkbox");
-  completeButton.setAttribute("id", "myCompleteButton");
+  completeButton.setAttribute("id", "c"+li.id);
+  completeButton.setAttribute("onClick","completeItem(this.id)");
   
-
-
 
   const myNode = document.createTextNode(newTask);
   li.appendChild(completeButton);
   li.appendChild(myNode);
   li.appendChild(deleteButton);
-
-  completeButton.onclick = function() {
-    if (myCompleteButton.checked == true){
-      li.style="text-decoration: line-through";
-    } else {
-      li.style="text-decoration: normal";
-    }
-  }
-
+  //console.log(li.id)
+  //console.log(deleteButton.id)
+  //console.log(completeButton.id)
 
 
   allTasks.appendChild(li);
   document.getElementById("myForm").reset();
+}
+
+
+
+function deleteItem(id) {
+  var item = document.getElementById(id);
+  document.getElementById("taskList").removeChild(item);
+}
+
+
+function completeItem(id) {
+  var item = document.getElementById(id);
+  let li = item.parentNode;
+  //console.log(item);
+
+
+  if (item.checked == true) {
+    li.style="text-decoration: line-through";
+  } else {
+    li.style="text-decoration: normal"
+  }
 }

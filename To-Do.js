6c2b5384a56c0de37b44a9410940850489a8e339
James Hirschfelder <james.hirschfelder@gmail.com>
2021-05-07 15:06:30 -0700

    styling added

--- To-Do.js
+++ To-Do.js
@@ -1,20 +1,15 @@
 const button = document.getElementById("mybutton");
 button.addEventListener('click',theNewTask);
 
-
-
-
 function theNewTask(e){
   e.preventDefault();
-
-  //add if duplicate alert
-  //} else {
 
   const allTasks = document.getElementById("taskList")
   const newTask = document.getElementById("taskName").value;
   
   const li = document.createElement("li");
   li.setAttribute("id", Date.now());
+  li.setAttribute("name", "incompleteTask");
 
   const deleteButton=document.createElement("button");
   deleteButton.innerHTML="X";
@@ -33,33 +28,26 @@ function theNewTask(e){
   li.appendChild(completeButton);
   li.appendChild(myNode);
   li.appendChild(deleteButton);
-  //console.log(li.id)
-  //console.log(deleteButton.id)
-  //console.log(completeButton.id)
-
 
   allTasks.appendChild(li);
   document.getElementById("myForm").reset();
 }
-
-
 
 function deleteItem(id) {
   var item = document.getElementById(id);
   document.getElementById("taskList").removeChild(item);
 }
 
-
 function completeItem(id) {
   var item = document.getElementById(id);
   let li = item.parentNode;
-  //console.log(item);
-
 
   if (item.checked == true) {
     li.style="text-decoration: line-through";
+    li.setAttribute("name", "completeTask");
   } else {
     li.style="text-decoration: normal"
+    li.setAttribute("name", "incompleteTask");
   }
 }
 
